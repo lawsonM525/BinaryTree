a227f7ad4b95e4be89d61e9ab72568c39bd61593
Michelle L <mlawson@smith.edu>
2023-04-03 10:05:27 -0400

    notas

---
+++ BinaryTree.java
@@ -0,0 +1,6 @@
+// free to do some research but come up with your own code
+// implement constructor, toString, and addNode
+// addNode should add a node to the tree in the correct location
+public class BinaryTree {
+    
+}
