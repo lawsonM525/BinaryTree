84f96a631b899a146e9f3b2909f2760d26121c31
Michelle L <mlawson@smith.edu>
2023-04-05 00:13:35 -0400

    Node class built

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,6 +1,8 @@
 // free to do some research but come up with your own code
 // implement constructor, toString, and addNode
 // addNode should add a node to the tree in the correct location
+//node information is an integer
 public class BinaryTree {
+    
     
 }
--- Node.java
+++ Node.java
@@ -1,3 +1,49 @@
 // has recursive pointer to both a right and a left child
 // has name or number or whatever label
 // recursion is key in the binary tree
+
+public class Node{
+    
+    //Class variables : data, left, right
+    private int data;
+    private Node left;
+    private Node right;
+
+    //Constructor
+    public Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    //Getters and Setters
+    public int getData(){
+        return this.data;
+    }
+
+    public void setData(int data){
+        this.data = data;
+    }
+
+    public Node getLeft(){
+        return this.left;
+    }
+
+    public void setLeft(Node left){
+        this.left = left;
+    }
+
+    public Node getRight(){
+        return this.right;
+    }
+
+    public void setRight(Node right){
+        this.right = right;
+    }
+
+    //toString
+    public String toString(){
+        return "Node: " + this.data;
+    }
+
+}
